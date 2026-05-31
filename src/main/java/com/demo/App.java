1b92ac7eadcb5ebfe4c9780943014ff259514d87
Michele Bettin <michele.bettin2002@gmail.com>
2026-05-31 21:22:40 +0200

    Refactoring 2

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -14,13 +14,6 @@ import java.util.Map;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
 public class App {
 
     public static void main(String[] args) throws Exception {
@@ -58,17 +51,13 @@ public class App {
 
     // Apache HTTP client
     public static void httpExample() throws Exception {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        
-        HttpGet request = new HttpGet("http://example.com");
-        
-        try (CloseableHttpResponse response = httpClient.execute(request)) {
-            HttpEntity entity = response.getEntity();
-            String body = EntityUtils.toString(entity);
-            System.out.println("HTTP response length: " + body.length());
-        } finally {
-            httpClient.close();
-        }
+        String response = Request.Get("http://example.com")
+                .connectTimeout(1000)
+                .socketTimeout(1000)
+                .execute()
+                .returnContent()
+                .asString();
+        System.out.println("HTTP response length: " + response.length());
     }
 
     // Commons IO usage
